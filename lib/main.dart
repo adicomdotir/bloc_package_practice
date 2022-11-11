0e32e36ba022fda00c3756504e8403ef6e02adb8
adicomdotir <adicom.ir@gmail.com>
2022-11-11 23:51:04 +0330

    Update README.md

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,23 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:testingbloc_practice/bloc/bloc_actions.dart';
-import 'package:testingbloc_practice/bloc/person.dart';
-import 'package:testingbloc_practice/bloc/persons_bloc.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) => PersonsBloc(),
-        child: const MyApp(),
-      ),
+      title: 'Flutter Demo',
+      home: HomePage(),
     ),
   );
 }
 
-Future<Iterable<Person>> getPersons(String url) => HttpClient()
-    .getUrl(Uri.parse(url))
-    .then((req) => req.close())
-    .then((response) => response.transform(utf8.decoder).join())
-    .then((str) => jsonDecode(str) as List<dynamic>)
-    .then((list) => list.map((e) => Person.fromJson(e)));
-
-extension Subscript<T> on Iterable<T> {
-  T? operator [](int index) => length > index ? elementAt(index) : null;
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bloc Practice'),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              TextButton(
-                onPressed: () {
-                  context.read<PersonsBloc>().add(
-                        const LoadPersonAction(
-                          url: persons1Url,
-                          loader: getPersons,
-                        ),
-                      );
-                },
-                child: const Text('Load json #1'),
-              ),
-              TextButton(
-                onPressed: () {
-                  context.read<PersonsBloc>().add(
-                        const LoadPersonAction(
-                          url: persons2Url,
-                          loader: getPersons,
-                        ),
-                      );
-                },
-                child: const Text('Load json #2'),
-              )
-            ],
-          ),
-          BlocBuilder<PersonsBloc, FetchResult?>(
-            buildWhen: (previous, current) {
-              return previous?.persons != current?.persons;
-            },
-            builder: (context, state) {
-              final persons = state?.persons;
-              if (persons == null) {
-                return const SizedBox();
-              }
-              return Expanded(
-                child: ListView.builder(
-                  itemCount: persons.length,
-                  itemBuilder: (context, index) {
-                    final person = persons[index]!;
-                    return ListTile(
-                      title: Text(person.name),
-                    );
-                  },
-                ),
-              );
-            },
-          )
-        ],
+        title: const Text('HomePage'),
       ),
     );
   }
